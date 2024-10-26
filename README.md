36ec08ba754601cd39d03354430c49e0318c9443
Gandalf-Le-Dev <matproz.gaming@gmail.com>
2024-10-27 00:10:23 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# aurora
+
+A Terminal string styling for zig.
+
+> [!NOTE]
+> This repository is heavily inspired by [tr1ckydev/chameleon](https://github.com/tr1ckydev/chameleon).
+
+## Installation
+
+## Usage
+
+Aurora is a **Runtime** string styler.
+
+## Documentation
+
+Check out the full documentation [here](https://github.com/gandalf-le-dev/aurora/blob/main/DOCUMENTATION.md).
+
+## `NO_COLOR` support
+
+> According to [no-color.org](https://no-color.org/), *"Command-line software which adds ANSI color to its output by default should check for a `NO_COLOR` environment variable that, when present and not an empty string (regardless of its value), prevents the addition of ANSI color."*
+>
+
+Aurora supports this standard only in it's **Runtime API** by detecting the presence of the *NO_COLOR* environment variable (regardless of it's value), if present disables any ANSI colors or styles, and can also be configured to not do so if you want to implement a different way of detection, although highly not recommended.
+
+## License
+
+This repository uses the MIT License. Check [LICENSE](https://github.com/gandalf-le-dev/aurora/blob/main/LICENSE) for full license text.
---
+++ src/main.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const Aurora = @import("aurora");
+const Config = Aurora.Config;
+
+pub fn main() !void {
+    const config: Config = Config{
+        .allocator = std.heap.page_allocator,
+        .detect_no_color = true,
+    };
+
+    var c: Aurora = Aurora.init(config);
+    var header = c.underline().bold().italic().blink().createPreset();
+
+    std.debug.print("\t\t  {s}{s}{s}{s}{s}{s}\n\n", .{
+        header.green().fmt("A", .{}),
+        header.blue().fmt("U", .{}),
+        header.cyan().fmt("R", .{}),
+        header.magenta().fmt("O", .{}),
+        header.red().fmt("R", .{}),
+        header.green().fmt("A", .{}),
+    });
+    std.debug.print("{s} {s} {s} {s} {s} {s}\n{s} {s} {s} {s} {s} {s} {s} {s}\n{s} {s} {s} {s} {s} {s}\n", .{
+        c.bold().fmt("bold", .{}),
+        c.dim().fmt("dim", .{}),
+        c.italic().fmt("italic", .{}),
+        c.underline().fmt("underline", .{}),
+        c.inverse().fmt("inverse", .{}),
+        c.strikethrough().fmt("strikethrough", .{}),
+        c.red().fmt("red", .{}),
+        c.green().fmt("green", .{}),
+        c.yellow().fmt("yellow", .{}),
+        c.blue().fmt("blue", .{}),
+        c.magenta().fmt("magenta", .{}),
+        c.cyan().fmt("cyan", .{}),
+        c.white().fmt("white", .{}),
+        c.gray().fmt("gray", .{}),
+        c.black().bgRed().fmt("bgRed", .{}),
+        c.black().bgGreen().fmt("bgGreen", .{}),
+        c.black().bgYellow().fmt("bgYellow", .{}),
+        c.black().bgBlue().fmt("bgBlue", .{}),
+        c.black().bgMagenta().fmt("bgMagenta", .{}),
+        c.black().bgCyan().fmt("bgCyan", .{}),
+    });
+}
